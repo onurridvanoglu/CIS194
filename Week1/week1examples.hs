ec757eee769f33b82a7e5734a55389a4340211d2
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-21 18:22:08 +0300

    add examples

---
+++ Week1/week1examples.hs
@@ -0,0 +1,16 @@
+-- Finding the range of the Int values
+biggestInt, smallestInt :: Int
+biggestInt = maxBound
+smallestInt = minBound
+
+reallyBig :: Integer
+reallyBig = 2 ^ (2 ^ (2 ^ (2 ^ 2)))
+
+-- Determining the digit number of "reallyBig"
+numDigits :: Int 
+numDigits = length (show reallyBig)
+
+-- Sum of number from 1 to n
+sumtorial :: Integer -> Integer
+sumtorial 0 = 0
+sumtorial n = n + sumtorial (n - 1)
